822c868e7164345f916a12668d43e57c96d2b235
Abel Cortes <abelsantiagocortes@gmail.com>
2021-04-15 19:39:08 -0500

    Img Upload and Load to Storage from AWS

--- app/src/main/java/com/example/pixelmanipulation/AmplifyActivity.java
+++ app/src/main/java/com/example/pixelmanipulation/AmplifyActivity.java
@@ -1,7 +1,10 @@
 package com.example.pixelmanipulation;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.Todo;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 
 public class AmplifyActivity extends AppCompatActivity {
 
     private TextView mTextView;
+    private ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +32,14 @@ public class AmplifyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_amplify);
 
         mTextView = (TextView) findViewById(R.id.text);
+        imgView =(ImageView) findViewById(R.id.imagensita);
 
             try {
                 // Add these lines to add the AWSApiPlugin plugins
                 Amplify.addPlugin(new AWSApiPlugin());
+                Amplify.addPlugin(new AWSCognitoAuthPlugin());
+
+                Amplify.addPlugin(new AWSS3StoragePlugin());
                 Amplify.configure(getApplicationContext());
 
                 Log.i("MyAmplifyApp", "Initialized Amplify");
@@ -35,13 +49,55 @@ public class AmplifyActivity extends AppCompatActivity {
 
         Todo todo = Todo.builder()
                 .name("passar tesis")
-                .description("tcon legardis")
+                .description("tcon legardisssssss")
                 .build();
 
         Amplify.API.mutate(
                 ModelMutation.create(todo),
                 response -> Log.i("MyAmplifyApp", "Added Todo with id: " + response.getData().getId()),
                 error -> Log.e("MyAmplifyApp", "Create failed", error)
+        );
+
+
+            File exampleFile = new File(getApplicationContext().getFilesDir(), "ExampleKey");
+
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(exampleFile));
+                writer.append("Example file contents");
+                writer.close();
+            } catch (Exception exception) {
+                Log.e("MyAmplifyApp", "Upload failed", exception);
+            }
+
+            Amplify.Storage.uploadFile(
+                    "ExampleKey",
+                    exampleFile,
+                    result -> Log.i("MyAmplifyApp", "Successfully uploaded: " + result.getKey()),
+                    storageFailure -> Log.e("MyAmplifyApp", "Upload failed", storageFailure)
+            );
+
+            //lee desde almacenamiento interno
+             Bitmap myBitmap = BitmapFactory.decodeFile(getApplicationContext().getFilesDir()+ "/download.jpeg");
+            imgView.setImageBitmap(myBitmap);
+
+            loadImage();
+    }
+
+    private void loadImage() {
+        // descarga y lee desde aws
+        Amplify.Storage.downloadFile(
+                "tomografia.jpeg",
+                new File(getApplicationContext().getFilesDir() + "/downlojagd.jpeg"),
+                result -> {
+                    Bitmap myBitmap = BitmapFactory.decodeFile(getApplicationContext().getFilesDir()+ "/download.jpeg");
+                    imgView.setImageBitmap(myBitmap);},
+                error -> Log.e("MyAmplifyApp",  "Download Failure", error)
+        );
+
+        Amplify.Storage.getUrl(
+                "tomografia.jpeg",
+                result -> Log.i("MyAmplifyApp", "Successfully generated: " + result.getUrl()),
+                error -> Log.e("MyAmplifyApp", "URL generation failure", error)
         );
 
 
